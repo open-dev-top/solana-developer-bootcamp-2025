e1671015f30c842fa8a2d7f9f358c4542fe27986
open-dev-top <opendev.top@gmail.com>
2025-02-03 01:09:33 +0800

    feat(p6): add create-nft.ts and verify-nft.ts

--- project-6/new-nft/README.md
+++ project-6/new-nft/README.md
@@ -8,7 +8,7 @@ npm i @metaplex-foundation/umi-bundle-defaults
 npm i @solana-developers/helpers
 ```
 
-## Run
+## Config
 
 ```shell
 solana config set --keypair ~/.config/solana/id.json
@@ -18,4 +18,34 @@ solana config get
 solana address
 
 npx esrun create-collections.ts
+```
+
+## Run
+
+### Create Collection
+
+``` shell
+alex@MacBook-Air new-nft % npx esrun create-collections.ts
+Loaded user AHdyMbVNGT8YVWCMZnfakVv53GwiJwvEFbkCYruZikZC
+Set up Umi instance for user
+Created Collection 📦! Address is https://explorer.solana.com/address/2WoYAJZ1qQEWVa21XmvAqBRQRRTtpCv9Dg3EBB4RHqsk?cluster=devnet
+```
+
+### Create NFT
+
+```shell
+alex@MacBook-Air new-nft % npx esrun create-nft.ts        
+Loaded user AHdyMbVNGT8YVWCMZnfakVv53GwiJwvEFbkCYruZikZC
+Set up Umi instance for user
+Creating NFT...
+🖼️ Created NFT! Address is https://explorer.solana.com/address/GjP5f8jz21q7cuCmCb2QrLTJRHPEw6cmyhMB7gg9qBrQ?cluster=devnet
+```
+
+### Verify NFT belong to Collection
+
+```shell
+alex@MacBook-Air new-nft % npx esrun verify-nft.ts 
+Loaded user AHdyMbVNGT8YVWCMZnfakVv53GwiJwvEFbkCYruZikZC
+Set up Umi instance for user
+✅ NFT GjP5f8jz21q7cuCmCb2QrLTJRHPEw6cmyhMB7gg9qBrQ verified as member of collection 2WoYAJZ1qQEWVa21XmvAqBRQRRTtpCv9Dg3EBB4RHqsk! See Explorer at https://explorer.solana.com/address/GjP5f8jz21q7cuCmCb2QrLTJRHPEw6cmyhMB7gg9qBrQ?cluster=devnet
 ```
---
+++ project-6/new-nft/verify-nft.ts
@@ -0,0 +1,60 @@
+import {
+  findMetadataPda,
+  mplTokenMetadata,
+  verifyCollectionV1,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+
+import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import { keypairIdentity, publicKey } from "@metaplex-foundation/umi";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL
+);
+
+console.log("Loaded user", user.publicKey.toBase58());
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("Set up Umi instance for user");
+
+// We could also dp
+const collectionAddress = publicKey(
+  "2WoYAJZ1qQEWVa21XmvAqBRQRRTtpCv9Dg3EBB4RHqsk"
+);
+
+const nftAddress = publicKey("GjP5f8jz21q7cuCmCb2QrLTJRHPEw6cmyhMB7gg9qBrQ");
+
+const transaction = await verifyCollectionV1(umi, {
+  metadata: findMetadataPda(umi, { mint: nftAddress }),
+  collectionMint: collectionAddress,
+  authority: umi.identity,
+});
+
+transaction.sendAndConfirm(umi);
+
+console.log(
+  `✅ NFT ${nftAddress} verified as member of collection ${collectionAddress}! See Explorer at ${getExplorerLink(
+    "address",
+    nftAddress,
+    "devnet"
+  )}`
+);
